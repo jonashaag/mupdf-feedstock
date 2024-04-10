b8b5e7f2a45076449533f065752f9d780552943a
Jan Janssen <jan-janssen@users.noreply.github.com>
2024-04-09 23:11:45 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -22,6 +22,6 @@ export VENV_FLAG=""
 #ls -lh ${PREFIX}/lib
 
 # build and install
-make prefix="${PREFIX}" shared=yes -j ${CPU_COUNT} all python
+make prefix="${PREFIX}" pydir="${SP_DIR}" shared=yes -j ${CPU_COUNT} all python
 # no make check
-make prefix="${PREFIX}" shared=yes install install-shared-python
+make prefix="${PREFIX}" pydir="${SP_DIR}" shared=yes install install-shared-python
